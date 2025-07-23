773360e8f9b0ad0738a7fb82cae84f593e95e8d0
Ishida Amuna <134792769+ishidaamna@users.noreply.github.com>
2025-07-23 20:41:33 +0900

    first commit

---
+++ src/components/NewProject.jsx
@@ -0,0 +1,65 @@
+import { useRef } from "react";
+import Input from "./Input";
+import Modal from "./Modal";
+
+export default function NewProject({ onAdd, onCancel }) {
+  const modal = useRef();
+  const title = useRef();
+  const description = useRef();
+  const dueDate = useRef();
+
+  function handleSave() {
+    const entertedTitle = title.current.value;
+    const entertedDescription = description.current.value;
+    const entertedDueDate = dueDate.current.value;
+
+    if (
+      entertedTitle.trim() === "" ||
+      entertedDescription.trim() === "" ||
+      entertedDueDate.trim() === ""
+    ) {
+      modal.current.open();
+      return;
+    }
+
+    onAdd({
+      title: entertedTitle,
+      description: entertedDescription,
+      dueDate: entertedDueDate,
+    });
+  }
+
+  return (
+    <>
+      <Modal ref={modal} buttonCaption="Okay" >
+        <h2 className='text-xl font-bold text-stone-700 mt-4 my-4'>Invalid Input</h2>
+        <p className='text-stone-600 mb-4'>Oops ... looks like you forgot to enter a value.</p>
+        <p className='text-stone-600 mb-4'>Please make sure you provide a valid value for every input field.</p>
+      </Modal>
+      <div className="w-[35rem] mt-16">
+        
+        <div>
+          <Input type="text" ref={title} label="Title" />
+          <Input ref={description} label="Description" textarea />
+          <Input type="date" ref={dueDate} label="Due Date" />
+        </div>
+        <menu className="flex items-center justify-end gap-4 my-4">
+          <li>
+            <button className="text-stone-800 hover:text-stone-950" 
+            onClick={onCancel}>
+              Cancel
+            </button>
+          </li>
+          <li>
+            <button
+              className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950"
+              onClick={handleSave}
+            >
+              Save
+            </button>
+          </li>
+        </menu>
+      </div>
+    </>
+  );
+}
