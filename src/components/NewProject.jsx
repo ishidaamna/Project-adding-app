2243bc6ebdf09f5d18e1b407a1b8e22e09627fee
Ishida Amuna <134792769+ishidaamna@users.noreply.github.com>
2025-07-25 14:21:51 +0900

    Update NewProject.jsx
    Made the code more readable.

--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -3,50 +3,51 @@ import Input from "./Input";
 import Modal from "./Modal";
 
 export default function NewProject({ onAdd, onCancel }) {
-  const modal = useRef();
-  const title = useRef();
-  const description = useRef();
-  const dueDate = useRef();
+  const modalRef = useRef();
+  const titleRef = useRef();
+  const descriptionRef = useRef();
+  const dueDateRef = useRef();
 
   function handleSave() {
-    const entertedTitle = title.current.value;
-    const entertedDescription = description.current.value;
-    const entertedDueDate = dueDate.current.value;
+    const title = titleRef.current?.value.trim();
+    const description = descriptionRef.current?.value.trim();
+    const dueDate = dueDateRef.current?.value.trim();
 
-    if (
-      entertedTitle.trim() === "" ||
-      entertedDescription.trim() === "" ||
-      entertedDueDate.trim() === ""
-    ) {
-      modal.current.open();
+    const isInvalid = !title || !description || !dueDate;
+
+    if (isInvalid) {
+      modalRef.current?.open();
       return;
     }
 
-    onAdd({
-      title: entertedTitle,
-      description: entertedDescription,
-      dueDate: entertedDueDate,
-    });
+    onAdd({ title, description, dueDate });
   }
 
   return (
     <>
-      <Modal ref={modal} buttonCaption="Okay" >
-        <h2 className='text-xl font-bold text-stone-700 mt-4 my-4'>Invalid Input</h2>
-        <p className='text-stone-600 mb-4'>Oops ... looks like you forgot to enter a value.</p>
-        <p className='text-stone-600 mb-4'>Please make sure you provide a valid value for every input field.</p>
+      <Modal ref={modalRef} buttonCaption="Okay">
+        <h2 className="text-xl font-bold text-stone-700 mt-4 my-4">
+          Invalid Input
+        </h2>
+        <p className="text-stone-600 mb-4">
+          Oops ... looks like you forgot to enter a value.
+        </p>
+        <p className="text-stone-600 mb-4">
+          Please make sure you provide a valid value for every input field.
+        </p>
       </Modal>
       <div className="w-[35rem] mt-16">
-        
         <div>
-          <Input type="text" ref={title} label="Title" />
-          <Input ref={description} label="Description" textarea />
-          <Input type="date" ref={dueDate} label="Due Date" />
+          <Input type="text" ref={titleRef} label="Title" />
+          <Input ref={descriptionRef} label="Description" textarea />
+          <Input type="date" ref={dueDateRef} label="Due Date" />
         </div>
         <menu className="flex items-center justify-end gap-4 my-4">
           <li>
-            <button className="text-stone-800 hover:text-stone-950" 
-            onClick={onCancel}>
+            <button
+              className="text-stone-800 hover:text-stone-950"
+              onClick={onCancel}
+            >
               Cancel
             </button>
           </li>
